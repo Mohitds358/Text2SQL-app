30d0566f6fb01b28897d34c000738c35be521773
Mohit <mohithotwanids@gmail.com>
2024-05-18 15:08:28 +0530

    Added test script for Text2SQL app.

---
+++ Test/test_script.py
@@ -0,0 +1,30 @@
+import time
+import pandas as pd
+from database_utils import init_database
+from generate_response import get_response
+
+
+def main():
+    test_queries = pd.read_csv("test_queries.csv")
+
+    db = init_database(user="root", password="mohit", host="localhost", port="3306", database="mimiciiiv14")
+
+    for index, row in test_queries.iterrows():
+        user_query = row['user_query']
+        chat_history = []
+
+        try:
+            # Get the response and the SQL query from the Text2SQL application
+            response = get_response(user_query, db, chat_history)
+
+            # Print the results
+            print(f"Query: {user_query}\nResponse: {response}\n")
+        except Exception as e:
+            print(f"Query: {user_query}\nError: {str(e)}\n")
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
+    print("Testing completed!!")
